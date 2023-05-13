2339c08d0ead1396beea0468126df31066f62787
SamAlrahhal <95586168+SamAlrahhal@users.noreply.github.com>
2023-05-13 12:19:19 +0200

    its not showing stuff in the home scheen

--- app/src/main/java/com/example/project/FirstFragment.java
+++ app/src/main/java/com/example/project/FirstFragment.java
@@ -15,6 +15,12 @@ import androidx.navigation.fragment.NavHostFragment;
 import com.example.project.databinding.FragmentFirstBinding;
 import org.jetbrains.annotations.NotNull;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,41 +30,48 @@ public class FirstFragment extends Fragment {
     private SQLiteDatabase database;
 
 
-    private List<String> getUpcomingBirthdays(String timePeriod) {
-        // Create a List to hold the names
-        List<String> names = new ArrayList<>();
+    public void getUpcomingBirthdays() {
+        // Get current date
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String currentDate = sdf.format(new Date());
 
-        // Create a SQL query
-        String query;
-        switch (timePeriod) {
-            case "today":
-                query = "SELECT name FROM person WHERE DATE(birthdate) = DATE('now')";
-                break;
-            case "week":
-                query = "SELECT name FROM person WHERE DATE(birthdate) BETWEEN DATE('now') AND DATE('now', '+7 day')";
-                break;
-            case "month":
-                query = "SELECT name FROM person WHERE DATE(birthdate) BETWEEN DATE('now') AND DATE('now', '+1 month')";
-                break;
-            default:
-                throw new IllegalArgumentException("Invalid time period: " + timePeriod);
-        }
+        // Get current date + 7 days (for "This week" section)
+        Calendar c = Calendar.getInstance();
+        c.setTime(new Date());
+        c.add(Calendar.DATE, 7);
+        String currentDatePlusWeek = sdf.format(c.getTime());
 
-        // Execute the query
+        // Get current month (for "This month" section)
+        String currentMonth = new SimpleDateFormat("MM").format(new Date());
+
+        // Query to get today's birthdays
+        String query = "SELECT * FROM " + DatabaseHelper.TABLE_NAME +
+                " WHERE strftime('%m-%d', " + DatabaseHelper.COLUMN_BIRTHDATE + ") = strftime('%m-%d', '" + currentDate + "')";
         Cursor cursor = database.rawQuery(query, null);
 
-        // Loop through the results and add them to the List
-        if (cursor.moveToFirst()) {
-            do {
-                names.add(cursor.getString(0));
-            } while (cursor.moveToNext());
-        }
-
+        // Loop over the cursor to get data...
         // Close the cursor
         cursor.close();
 
-        return names;
+        // Query to get this week's birthdays
+        query = "SELECT * FROM " + DatabaseHelper.TABLE_NAME +
+                " WHERE strftime('%m-%d', " + DatabaseHelper.COLUMN_BIRTHDATE + ") > strftime('%m-%d', '" + currentDate + "') AND strftime('%m-%d', " + DatabaseHelper.COLUMN_BIRTHDATE + ") < strftime('%m-%d', '" + currentDatePlusWeek + "')";
+        cursor = database.rawQuery(query, null);
+
+        // Loop over the cursor to get data...
+        // Close the cursor
+        cursor.close();
+
+        // Query to get this month's birthdays
+        query = "SELECT * FROM " + DatabaseHelper.TABLE_NAME +
+                " WHERE strftime('%m', " + DatabaseHelper.COLUMN_BIRTHDATE + ") = '" + currentMonth + "'";
+        cursor = database.rawQuery(query, null);
+
+        // Loop over the cursor to get data...
+        // Close the cursor
+        cursor.close();
     }
+
 
 
     @Override
@@ -68,20 +81,26 @@ public class FirstFragment extends Fragment {
     ) {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
-        return binding.getRoot();
 
+        // Initialize the database
+        DatabaseHelper dbHelper = new DatabaseHelper(getContext());
+        database = dbHelper.getReadableDatabase();
+
+        return binding.getRoot();
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        // Initialize the database
+        DatabaseHelper dbHelper = new DatabaseHelper(getContext());
+        database = dbHelper.getReadableDatabase();
+
+        // Call your method here to fetch data from the database
+        getUpcomingBirthdays();
+
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
 
 
 }
