3c8939eaa2c90579ad727098433601c5e0d27546
SamAlrahhal <95586168+SamAlrahhal@users.noreply.github.com>
2023-05-13 20:17:23 +0200

    stuff is wack

--- app/src/main/java/com/example/project/FirstFragment.java
+++ app/src/main/java/com/example/project/FirstFragment.java
@@ -7,27 +7,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.project.databinding.FragmentFirstBinding;
-import com.example.project.databinding.UpcomingBirthdaysBinding;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
     private SQLiteDatabase database;
-
-    // Add bindings for each included layout
-    private UpcomingBirthdaysBinding todayBirthdaysBinding;
-    private UpcomingBirthdaysBinding weekBirthdaysBinding;
-    private UpcomingBirthdaysBinding monthBirthdaysBinding;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -43,10 +40,6 @@ public class FirstFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        todayBirthdaysBinding = UpcomingBirthdaysBinding.bind(view.findViewById(R.id.today_birthdays));
-        weekBirthdaysBinding = UpcomingBirthdaysBinding.bind(view.findViewById(R.id.week_birthdays));
-        monthBirthdaysBinding = UpcomingBirthdaysBinding.bind(view.findViewById(R.id.month_birthdays));
 
         // Call your method here to fetch data from the database
         getUpcomingBirthdays();
@@ -67,16 +60,18 @@ public class FirstFragment extends Fragment {
         // Get current month (for "This month" section)
         String currentMonth = new SimpleDateFormat("MM").format(new Date());
 
+
+
         // Query to get today's birthdays
         String query = "SELECT * FROM " + DatabaseHelper.TABLE_NAME +
                 " WHERE strftime('%m-%d', " + DatabaseHelper.COLUMN_BIRTHDATE + ") = strftime('%m-%d', '" + currentDate + "')";
         Cursor cursor = database.rawQuery(query, null);
 
-        // Fetch data from cursor and update the appropriate TextView
-        StringBuilder todayBirthdays = new StringBuilder();
+        // Fetch data from cursor and update the appropriate ListView
+        ArrayList<String> todayBirthdays = new ArrayList<>();
         while (cursor.moveToNext()) {
             String name = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_NAME));
-            todayBirthdays.append(name).append("\n");
+            todayBirthdays.add(name);
         }
         cursor.close();
 
@@ -85,34 +80,42 @@ public class FirstFragment extends Fragment {
                 " WHERE strftime('%m-%d', " + DatabaseHelper.COLUMN_BIRTHDATE + ") > strftime('%m-%d', '" + currentDate + "') AND strftime('%m-%d', " + DatabaseHelper.COLUMN_BIRTHDATE + ") <= strftime('%m-%d', '" + currentDatePlusWeek + "')";
         cursor = database.rawQuery(query, null);
 
-        StringBuilder weekBirthdays = new StringBuilder();  // Create a StringBuilder for week birthdays
+        ArrayList<String> weekBirthdays = new ArrayList<>();
         while (cursor.moveToNext()) {
-            @SuppressLint("Range") String name = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_NAME));
-            weekBirthdays.append(name).append("\n");  // Append to week birthdays
+            String name = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_NAME));
+            weekBirthdays.add(name);
         }
         cursor.close();
+
+
 
         // Query to get this month's birthdays
         query = "SELECT * FROM " + DatabaseHelper.TABLE_NAME +
                 " WHERE strftime('%m', " + DatabaseHelper.COLUMN_BIRTHDATE + ") = '" + currentMonth + "'";
         cursor = database.rawQuery(query, null);
 
-        StringBuilder monthBirthdays = new StringBuilder();  // Create a StringBuilder for month birthdays
+        ArrayList<String> monthBirthdays = new ArrayList<>();
         while (cursor.moveToNext()) {
-            @SuppressLint("Range") String name = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_NAME));
-            monthBirthdays.append(name).append("\n");  // Append to month birthdays
+            String name = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_NAME));
+            monthBirthdays.add(name);
         }
         cursor.close();
 
-        // Fetch data from cursor and update the appropriate TextView
-        TextView todayBirthdaysTextView = todayBirthdaysBinding.birthdays;
-        TextView weekBirthdaysTextView = weekBirthdaysBinding.birthdays;
-        TextView monthBirthdaysTextView = monthBirthdaysBinding.birthdays;
 
-        // Set the text for each TextView
-        todayBirthdaysTextView.setText(todayBirthdays.toString());
-        weekBirthdaysTextView.setText(weekBirthdays.toString());
-        monthBirthdaysTextView.setText(monthBirthdays.toString());
+        ArrayAdapter<String> monthAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, monthBirthdays);
+        ListView monthListView = binding.monthBirthdays.monthBirthdayList;
+        monthListView.setAdapter(monthAdapter);
+
+/*
+        ArrayAdapter<String> weekAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, weekBirthdays);
+        ListView weekListView = binding.weekBirthdays.weekBirthdayList;
+        weekListView.setAdapter(weekAdapter);
+
+        ArrayAdapter<String> todayAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, todayBirthdays);
+        ListView todayListView = binding.weekBirthdays.weekBirthdayList;
+        todayListView.setAdapter(todayAdapter);
+
+
+ */
     }
-
 }
