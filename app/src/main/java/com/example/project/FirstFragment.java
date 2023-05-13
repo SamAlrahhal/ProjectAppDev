ce6e39e0e9c9ae10d217855f0216f513f85aa2f9
SamAlrahhal <95586168+SamAlrahhal@users.noreply.github.com>
2023-05-13 15:30:47 +0200

    getting somewhere

--- app/src/main/java/com/example/project/FirstFragment.java
+++ app/src/main/java/com/example/project/FirstFragment.java
@@ -1,33 +1,56 @@
 package com.example.project;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
-import com.example.project.databinding.FragmentFirstBinding;
-import org.jetbrains.annotations.NotNull;
 
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import com.example.project.databinding.FragmentFirstBinding;
+import com.example.project.databinding.UpcomingBirthdaysBinding;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.ArrayList;
-import java.util.List;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
     private SQLiteDatabase database;
+
+    // Add bindings for each included layout
+    private UpcomingBirthdaysBinding todayBirthdaysBinding;
+    private UpcomingBirthdaysBinding weekBirthdaysBinding;
+    private UpcomingBirthdaysBinding monthBirthdaysBinding;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+
+        // Initialize the database
+        DatabaseHelper dbHelper = new DatabaseHelper(getContext());
+        database = dbHelper.getReadableDatabase();
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        todayBirthdaysBinding = UpcomingBirthdaysBinding.bind(view.findViewById(R.id.today_birthdays));
+        weekBirthdaysBinding = UpcomingBirthdaysBinding.bind(view.findViewById(R.id.week_birthdays));
+        monthBirthdaysBinding = UpcomingBirthdaysBinding.bind(view.findViewById(R.id.month_birthdays));
+
+        // Call your method here to fetch data from the database
+        getUpcomingBirthdays();
+    }
 
 
     public void getUpcomingBirthdays() {
@@ -49,17 +72,24 @@ public class FirstFragment extends Fragment {
                 " WHERE strftime('%m-%d', " + DatabaseHelper.COLUMN_BIRTHDATE + ") = strftime('%m-%d', '" + currentDate + "')";
         Cursor cursor = database.rawQuery(query, null);
 
-        // Loop over the cursor to get data...
-        // Close the cursor
+        // Fetch data from cursor and update the appropriate TextView
+        StringBuilder todayBirthdays = new StringBuilder();
+        while (cursor.moveToNext()) {
+            String name = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_NAME));
+            todayBirthdays.append(name).append("\n");
+        }
         cursor.close();
 
         // Query to get this week's birthdays
         query = "SELECT * FROM " + DatabaseHelper.TABLE_NAME +
-                " WHERE strftime('%m-%d', " + DatabaseHelper.COLUMN_BIRTHDATE + ") > strftime('%m-%d', '" + currentDate + "') AND strftime('%m-%d', " + DatabaseHelper.COLUMN_BIRTHDATE + ") < strftime('%m-%d', '" + currentDatePlusWeek + "')";
+                " WHERE strftime('%m-%d', " + DatabaseHelper.COLUMN_BIRTHDATE + ") > strftime('%m-%d', '" + currentDate + "') AND strftime('%m-%d', " + DatabaseHelper.COLUMN_BIRTHDATE + ") <= strftime('%m-%d', '" + currentDatePlusWeek + "')";
         cursor = database.rawQuery(query, null);
 
-        // Loop over the cursor to get data...
-        // Close the cursor
+        StringBuilder weekBirthdays = new StringBuilder();  // Create a StringBuilder for week birthdays
+        while (cursor.moveToNext()) {
+            @SuppressLint("Range") String name = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_NAME));
+            weekBirthdays.append(name).append("\n");  // Append to week birthdays
+        }
         cursor.close();
 
         // Query to get this month's birthdays
@@ -67,40 +97,22 @@ public class FirstFragment extends Fragment {
                 " WHERE strftime('%m', " + DatabaseHelper.COLUMN_BIRTHDATE + ") = '" + currentMonth + "'";
         cursor = database.rawQuery(query, null);
 
-        // Loop over the cursor to get data...
-        // Close the cursor
+        StringBuilder monthBirthdays = new StringBuilder();  // Create a StringBuilder for month birthdays
+        while (cursor.moveToNext()) {
+            @SuppressLint("Range") String name = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_NAME));
+            monthBirthdays.append(name).append("\n");  // Append to month birthdays
+        }
         cursor.close();
+
+        // Fetch data from cursor and update the appropriate TextView
+        TextView todayBirthdaysTextView = todayBirthdaysBinding.birthdays;
+        TextView weekBirthdaysTextView = weekBirthdaysBinding.birthdays;
+        TextView monthBirthdaysTextView = monthBirthdaysBinding.birthdays;
+
+        // Set the text for each TextView
+        todayBirthdaysTextView.setText(todayBirthdays.toString());
+        weekBirthdaysTextView.setText(weekBirthdays.toString());
+        monthBirthdaysTextView.setText(monthBirthdays.toString());
     }
-
-
-
-    @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-
-        binding = FragmentFirstBinding.inflate(inflater, container, false);
-
-        // Initialize the database
-        DatabaseHelper dbHelper = new DatabaseHelper(getContext());
-        database = dbHelper.getReadableDatabase();
-
-        return binding.getRoot();
-    }
-
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        // Initialize the database
-        DatabaseHelper dbHelper = new DatabaseHelper(getContext());
-        database = dbHelper.getReadableDatabase();
-
-        // Call your method here to fetch data from the database
-        getUpcomingBirthdays();
-
-    }
-
-
 
 }
