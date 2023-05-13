f1e7f4f0c1e0b9539890038b173081c17bc0b4b5
SamAlrahhal <95586168+SamAlrahhal@users.noreply.github.com>
2023-05-13 11:31:18 +0200

    de task bar geeft de juist namen

---
+++ app/src/main/java/com/example/project/BirthdayListAdapter.java
@@ -0,0 +1,33 @@
+package com.example.project;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class BirthdayListAdapter extends ArrayAdapter<String> {
+
+    public BirthdayListAdapter(@NonNull Context context, @NonNull List<String> names) {
+        super(context, 0, names);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        }
+
+        TextView textView = convertView.findViewById(android.R.id.text1);
+        textView.setText(getItem(position));
+
+        return convertView;
+    }
+}
