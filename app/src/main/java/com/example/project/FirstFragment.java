07caeb00da7f002ba4075e44916480f9652c3d7d
SamAlrahhal <95586168+SamAlrahhal@users.noreply.github.com>
2023-05-13 11:36:49 +0200

    de task bar geeft de juist namen

--- app/src/main/java/com/example/project/FirstFragment.java
+++ app/src/main/java/com/example/project/FirstFragment.java
@@ -1,6 +1,8 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -13,9 +15,51 @@ import androidx.navigation.fragment.NavHostFragment;
 import com.example.project.databinding.FragmentFirstBinding;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    private SQLiteDatabase database;
+
+
+    private List<String> getUpcomingBirthdays(String timePeriod) {
+        // Create a List to hold the names
+        List<String> names = new ArrayList<>();
+
+        // Create a SQL query
+        String query;
+        switch (timePeriod) {
+            case "today":
+                query = "SELECT name FROM person WHERE DATE(birthdate) = DATE('now')";
+                break;
+            case "week":
+                query = "SELECT name FROM person WHERE DATE(birthdate) BETWEEN DATE('now') AND DATE('now', '+7 day')";
+                break;
+            case "month":
+                query = "SELECT name FROM person WHERE DATE(birthdate) BETWEEN DATE('now') AND DATE('now', '+1 month')";
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid time period: " + timePeriod);
+        }
+
+        // Execute the query
+        Cursor cursor = database.rawQuery(query, null);
+
+        // Loop through the results and add them to the List
+        if (cursor.moveToFirst()) {
+            do {
+                names.add(cursor.getString(0));
+            } while (cursor.moveToNext());
+        }
+
+        // Close the cursor
+        cursor.close();
+
+        return names;
+    }
+
 
     @Override
     public View onCreateView(
