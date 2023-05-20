9ce4764314a72609a3059c1122ad5f136faa07eb
SamAlrahhal <95586168+SamAlrahhal@users.noreply.github.com>
2023-05-20 16:38:36 +0200

    Than you J-man

--- app/src/main/java/com/example/project/DatabaseHelper.java
+++ app/src/main/java/com/example/project/DatabaseHelper.java
@@ -17,7 +17,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COLUMN_NAME = "name";
     public static final String COLUMN_BIRTHDATE = "birthdate";
     public static final String COLUMN_PHONE_NUMBER = "phoneNumber";
-    public static final int DATABASE_VERSION = 2;
+    public static final int DATABASE_VERSION = 3;
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
