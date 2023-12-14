eae6637daa607331e42f8a07630ab5fcea035c4d
Sam Al-Rahhal <95586168+SamAlrahhal@users.noreply.github.com>
2023-12-14 12:28:27 +0100

    Testcases work

--- app/src/androidTest/java/com/example/project/ShowAllTest.java
+++ app/src/androidTest/java/com/example/project/ShowAllTest.java
@@ -1,5 +1,4 @@
 package com.example.project;
-import androidx.annotation.ContentView;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Rule;
@@ -8,33 +7,23 @@ import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
-public class AddPerson {
+public class ShowAllTest {
     @Rule
     public ActivityScenarioRule<MainActivity> mActivityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+
     @Test
     public void show_all_ui_check() {
         // Click on the add person button
         // Check that the add person page is displayed
-        onView(withId(androidx.constraintlayout.widget.R.id.home)).perform(click());
+        onView(withId(R.id.home)).perform(click());
         onView(withId(R.id.showAll)).perform(click());
 
         onView(withId(R.id.showAll2)).check(matches(isDisplayed()));
-
-
     }
-
-    @Test
-    public void add_person() {
-        // Click on the add person button
-        // Check that the add person page is displayed
-        onView(withId(R.id.fab)).perform(click());
-
-        onView(withId(R.id.addPerson2)).check(matches(isDisplayed()));
-    }
-
 }
