cb2c7054a14d15de23beb865b19b152f2c0d8934
SamAlrahhal <95586168+SamAlrahhal@users.noreply.github.com>
2023-12-14 21:57:43 +0100

    testcase

--- app/src/androidTest/java/com/example/project/AddPersonTest.java
+++ app/src/androidTest/java/com/example/project/AddPersonTest.java
@@ -3,11 +3,13 @@ import androidx.fragment.app.Fragment;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
@@ -29,21 +31,20 @@ public class AddPersonTest {
 
         //put info into fields
         onView(withId(R.id.editTextName)).perform(click()).perform(typeText(name),closeSoftKeyboard());
-        onView(withId(R.id.editBirthdate)).perform(click()).perform(typeText(birthday),closeSoftKeyboard());
-        onView(withId(R.id.editPhoneNumber)).perform(click()).perform(typeText(phone),closeSoftKeyboard());
+        onView(withId(R.id.editTextBirthdate)).perform(click()).perform(typeText(birthday),closeSoftKeyboard());
+        onView(withId(R.id.editTextPhoneNumber)).perform(click()).perform(typeText(phone),closeSoftKeyboard());
         onView(withId(R.id.buttonAddPerson)).perform(click());
 
         //check that the person was added
-        onView(withId(R.id.home)).perform(click());
-
-        onView(withId(R.id.showAll2)).perform(click());
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().getTargetContext());
+        onView(withText("Show all")).perform(click());
 
         //scroll to bottom of list
-        onView(withId(R.id.recyclerView)).perform(RecyclerViewActions.scrollToLastPosition());
+        //onView(withId(R.id.recyclerView)).perform(RecyclerViewActions.scrollToLastPosition());
 
 
         //check that the person with the name was added
-        onView(withId(R.id.recyclerView)).check(matches(hasDescendant(withText(name))));
+        onView(withText("test")).check(matches((withText(name))));
 
     }
 
--- app/src/androidTest/java/com/example/project/ShowAllTest.java
+++ app/src/androidTest/java/com/example/project/ShowAllTest.java
@@ -1,16 +1,19 @@
 package com.example.project;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+
+import android.util.Log;
 
 @RunWith(AndroidJUnit4.class)
 public class ShowAllTest {
@@ -20,10 +23,17 @@ public class ShowAllTest {
     @Test
     public void show_all_ui_check() {
         // Click on the add person button
-        // Check that the add person page is displayed
-        onView(withId(R.id.home)).perform(click());
-        onView(withId(R.id.showAll)).perform(click());
+        // Check that the add person button is displayed
 
-        onView(withId(R.id.showAll2)).check(matches(isDisplayed()));
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().getTargetContext());
+        onView(withText("Show all")).perform(click());
+
+        // Check that the show all button is displayed
+        onView(withText("Show all")).check(matches(withText("Show all")));
+
+
+
+
+
     }
 }
